2a3b80f57d62c2474bce3499f6901f1c4478b335
Nyll <52743522+NyllRE@users.noreply.github.com>
2020-10-29 15:41:33 +0300

    added new thing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <img alt="Vue logo" src="https://v3.vuejs.org/logo.png" />
-  <h1 v-text="title"></h1>
-  <p v-html="magic"></p>
+	<img alt="Vue logo" src="https://v3.vuejs.org/logo.png" />
+	<h1 v-text="title"></h1>
+	<p v-html="magic"></p>
 </template>
 
 
 <script>
 export default {
-  data() {
-    return {
-      title: "First Vue 3 app!",
-      magic: "<h1>lol</h1>"
-    };
-  }
+	data() {
+		return {
+			title: 'First Vue 3 app!',
+			magic: '<h1>this is a new thing</h1>'
+		};
+	}
 };
 </script>
 
@@ -22,11 +22,11 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
