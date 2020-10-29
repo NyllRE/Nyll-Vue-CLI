5b615a52b6db101d8b293212fcc9814660eb303e
Nyll <52743522+NyllRE@users.noreply.github.com>
2020-10-29 14:58:40 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{ title.toUpperCase() }}</h1>
+  <p v-html="magic"></p>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: "First Vue 3 app!",
+      magic: "<h1>lol</h1>"
+    };
+  }
+};
+</script>
+
+
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
